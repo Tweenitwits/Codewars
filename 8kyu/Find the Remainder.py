b7aeb8768c16fa0595e40d48201b9d75b76aa6dd
Юрий Кожухов <tweenitwits@yandex.ru>
2023-09-18 13:44:21 +0300

    16 day

---
+++ 8kyu/Find the Remainder.py
@@ -0,0 +1,30 @@
+"""
+Task:
+Write a function that accepts two integers and returns the remainder of dividing the larger value by the smaller value.
+
+Division by zero should return an empty value.
+
+Examples:
+n = 17
+m = 5
+result = 2 (remainder of `17 / 5`)
+
+n = 13
+m = 72
+result = 7 (remainder of `72 / 13`)
+
+n = 0
+m = -1
+result = 0 (remainder of `0 / -1`)
+
+n = 0
+m = 1
+result - division by zero (refer to the specifications on how to handle this in your language)
+"""
+
+def remainder(a,b):
+    if a > b and b != 0: return a % b
+    elif b > a and a != 0: return b % a
+    elif a == b and a != 0 and b != 0: return 0
+    else:
+        return None
